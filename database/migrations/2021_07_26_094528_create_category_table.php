6d7f51c9ea623443fe02e3fa95187f333ba4bfca
johnayuk <john1234ayuk@gmail.com>
2022-03-05 20:31:18 +0100

    made a correction to database table

--- database/migrations/2021_07_26_094528_create_category_table.php
+++ database/migrations/2021_07_26_094528_create_category_table.php
@@ -16,7 +16,7 @@ class CreateCategoryTable extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('slug');
             $table->timestamps();
         });
     }
